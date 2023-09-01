d0cb87a0662cc40e89c0389c44734ccab61de292
davi.lemos <1davi.gabriel1@gmail.com>
2023-09-01 16:29:00 -0300

    01/09

---
+++ src/app/services/post-connection.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+import { FormConnection } from '../Models/FormConnection';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostConnectionService {
+
+  constructor() { }
+}
