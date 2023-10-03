e0f9cb61f3c00bce380dbe56f6c5474caf648e5d
davi.lemos <1davi.gabriel1@gmail.com>
2023-10-03 17:20:03 -0300

    feat: downloading .cs files from base64

--- src/app/comps/classes-list/classes-list.component.ts
+++ src/app/comps/classes-list/classes-list.component.ts
@@ -9,14 +9,19 @@ import { GeneratedClass } from 'src/app/Models/GeneratedClass';
 })
 export class ClassesListComponent {
   // @Input() databaseName = 'banco de dados';
+  generateURI(base: string){
+    return `data:file/cs;base64,${base}`
+  }
+
+  linkURI = this.generateURI("dXNpbmcgU3lzdGVtOw0KdXNpbmcgU3lzdGVtLkNvbGxlY3Rpb25zLkdlbmVyaWM7DQoKbmFtZXNwYWNlIHNpc2NvbS50ZXN0ZTsKDQpwdWJsaWMgY2xhc3MgU2V0b3IKewogCWRvdWJsZSBDT0RJR09TRVRPUiB7IGdldDsgc2V0OyB9DQoJc3RyaW5nIE5PTUVTRVRPUiB7IGdldDsgc2V0OyB9DQoJRGF0ZVRpbWUgREFUQUNBREFTVFJPIHsgZ2V0OyBzZXQ7IH0NCglEYXRlVGltZSBEQVRBSU5BVElWTyB7IGdldDsgc2V0OyB9DQogCn0NCg0K");
 
   tables: Array<GeneratedClass> = [
-    {name:'tabela1',download:'#'},
-    {name:'tabela2',download:'#'},
-    {name:'tabela3',download:'#'},
-    {name:'tabela4',download:'#'},
-    {name:'tabela5',download:'#'},
-    {name:'teste',download:'#'},
+    {name:'tabela1.cs',download:this.linkURI},
+    {name:'tabela2.cs',download:'#'},
+    {name:'tabela3.cs',download:'#'},
+    {name:'tabela4.cs',download:'#'},
+    {name:'tabela5.cs',download:'#'},
+    {name:'teste.cs',download:'#'},
 ]
 
   
