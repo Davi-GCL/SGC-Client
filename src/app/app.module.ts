451d7609663a0fbf66f818251c1233dc46b1aadb
davi.lemos <1davi.gabriel1@gmail.com>
2023-10-05 16:57:45 -0300

    feat http methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, NgModel , ReactiveFormsModule } from '@angular/forms';
 
+import { HttpClientModule } from '@angular/common/http'
+
 import { AppComponent } from './app.component';
 import { TableListComponent } from './comps/table-list/table-list.component';
 import { FilterPipe } from './filter.pipe';
--- src/app/services/get-tables.service.ts
+++ src/app/services/get-tables.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { FormConnection } from '../Models/FormConnection';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +10,12 @@ export class GetTablesService {
 
   sgbd:any
   connString:any
+  apiUrl = "https://localhost:7029/swagger/index.html";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  fetchAll(form:FormConnection): Observable<any> {
+
+    return this.http.post(this.apiUrl,{});
+  }
 }
